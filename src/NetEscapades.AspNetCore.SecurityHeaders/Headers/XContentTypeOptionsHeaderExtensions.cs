eeb345f3cd77d8da4ffece7fdf2dbbf6b926847d
Sock <sock@andrewlock.net>
2016-08-12 07:46:41 +0100

    Move extension method namespaces to root
    Should aid discovery and didn't make sense to be in 'Infrastructure' anyway

--- src/NetEscapades.AspNetCore.SecurityHeaders/Headers/XContentTypeOptionsHeaderExtensions.cs
+++ src/NetEscapades.AspNetCore.SecurityHeaders/Headers/XContentTypeOptionsHeaderExtensions.cs
@@ -1,4 +1,6 @@
-namespace NetEscapades.AspNetCore.SecurityHeaders.Infrastructure
+using NetEscapades.AspNetCore.SecurityHeaders.Infrastructure;
+
+namespace NetEscapades.AspNetCore.SecurityHeaders
 {
     /// <summary>
     /// Extension methods for adding a <see cref="XContentTypeOptionsHeader" /> to a <see cref="HeaderPolicyCollection" />
