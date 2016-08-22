516a92a324b4d652d14bdfaacb2909a302ac4817
Sock <sock@andrewlock.net>
2016-08-22 17:52:16 +0100

    Add RemoveCustomHeader option

---
+++ src/NetEscapades.AspNetCore.SecurityHeaders/Headers/RemoveCustomHeader.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+
+namespace NetEscapades.AspNetCore.SecurityHeaders.Infrastructure
+{
+    /// <summary>
+    /// Header value for Server header
+    /// </summary>
+    public class RemoveCustomHeader : HeaderPolicyBase
+    {
+        /// <summary>
+        /// /// Create a new instance of the remove custom header policy
+        /// </summary>
+        /// <param name="header">The string value of the header to remove</param>
+        public RemoveCustomHeader(string header) : base(string.Empty)
+        {
+            Header = header;
+        }
+
+        /// <inheritdoc />
+        public override string Header { get; }
+
+        /// <inheritdoc />
+        protected override void EvaluateHttpRequest(HttpContext context, CustomHeadersResult result)
+        {
+            EvaluateRequest(context, result);
+        }
+
+        /// <inheritdoc />
+        protected override void EvaluateHttpsRequest(HttpContext context, CustomHeadersResult result)
+        {
+            EvaluateRequest(context, result);
+        }
+
+        private void EvaluateRequest(HttpContext context, CustomHeadersResult result)
+        {
+            result.RemoveHeaders.Add(Header);
+        }
+    }
+}
