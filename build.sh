0c73be7c1c351627da850877f43adde6b14440c3
Andrew Lock <andrewlock.net@gmail.com>
2017-04-13 23:12:35 +0100

    Update ci builds

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ dotnet restore
 
 # Ideally we would use theis command to test netcoreapp and net451 so restrict for now 
 # but currently donsn't work due to https://github.com/dotnet/cli/issues/3073 
-dotnet test ./test/NetEscapades.AspNetCore.SecurityHeaders.Test -c Release -f netcoreapp1.0
+dotnet test ./test/NetEscapades.AspNetCore.SecurityHeaders.Test/NetEscapades.AspNetCore.SecurityHeaders.Test.csproj -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
-dotnet build ./test/NetEscapades.AspNetCore.SecurityHeaders.Test -c Release -f net451
+dotnet build ./test/NetEscapades.AspNetCore.SecurityHeaders.Test/NetEscapades.AspNetCore.SecurityHeaders.Test.csproj -c Release -f net451
 
 mono \
 ./test/NetEscapades.AspNetCore.SecurityHeaders.Test/bin/Release/net451/*/dotnet-test-xunit.exe \
@@ -25,4 +25,4 @@ mono \
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision) 
 
-dotnet pack ./src/NetEscapades.AspNetCore.SecurityHeaders -c Release -o ./artifacts --version-suffix=$revision
+dotnet pack ./src/NetEscapades.AspNetCore.SecurityHeaders/NetEscapades.AspNetCore.SecurityHeaders.csproj -c Release -o ./artifacts --version-suffix=$revision
