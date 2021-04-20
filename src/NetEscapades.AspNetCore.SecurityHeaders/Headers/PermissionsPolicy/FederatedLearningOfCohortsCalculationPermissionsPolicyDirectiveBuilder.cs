cc203565d293b11f132493cd9cd4e48d77c5c294
Jeremy Lindsay <jeremylindsayni@users.noreply.github.com>
2021-04-20 21:07:31 +0100

    Update src/NetEscapades.AspNetCore.SecurityHeaders/Headers/PermissionsPolicy/FederatedLearningOfCohortsCalculationPermissionsPolicyDirectiveBuilder.cs
    Gah - sorry I missed this, thank you for catching this!
    
    Co-authored-by: Andrew Lock <andrewlock.net@gmail.com>

--- src/NetEscapades.AspNetCore.SecurityHeaders/Headers/PermissionsPolicy/FederatedLearningOfCohortsCalculationPermissionsPolicyDirectiveBuilder.cs
+++ src/NetEscapades.AspNetCore.SecurityHeaders/Headers/PermissionsPolicy/FederatedLearningOfCohortsCalculationPermissionsPolicyDirectiveBuilder.cs
@@ -1,7 +1,7 @@
 ﻿namespace NetEscapades.AspNetCore.SecurityHeaders.Headers.PermissionsPolicy
 {
     /// <summary>
-    /// Controls whether the current document is allowed to use
+    /// Controls whether the current document is allowed to make a FLoC calculation
     /// If disabled in a document, the promise returned by <code>interestCohort()</code>
     /// must reject with a DOMException parameter.
     /// </summary>
