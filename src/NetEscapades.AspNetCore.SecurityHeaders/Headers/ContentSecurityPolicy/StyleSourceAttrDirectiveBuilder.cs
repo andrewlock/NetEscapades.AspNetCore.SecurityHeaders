6992bf42c910b8127b368f9289f765a12d5011b4
Thomas <37881874+ThomasBjallas@users.noreply.github.com>
2024-03-19 20:26:24 +0100

    I have correct a few Copy/Paste errors in the documention of the StyleSourceAttr and StyleSourceElem directives. (#152)

--- src/NetEscapades.AspNetCore.SecurityHeaders/Headers/ContentSecurityPolicy/StyleSourceAttrDirectiveBuilder.cs
+++ src/NetEscapades.AspNetCore.SecurityHeaders/Headers/ContentSecurityPolicy/StyleSourceAttrDirectiveBuilder.cs
@@ -1,11 +1,10 @@
 namespace NetEscapades.AspNetCore.SecurityHeaders.Headers.ContentSecurityPolicy;
 
 /// <summary>
-/// The style-src-attr directive specifies valid sources for stylesheet &lt;style&gt; elements
-/// and &lt;link&gt; elements with rel="stylesheet".
+/// The style-src-attr directive specifies valid sources for inline styles applied to individual DOM elements.
 ///
-/// The directive does not set valid sources for inline style attributes; these are
-/// set using style-src-attr (and valid sources for all styles may be set with style-src).
+/// The directive does not set valid sources for &lt;style&gt; elements and &lt;link&gt; elements with rel="stylesheet".
+/// These are set using style-src-elem (and valid sources for all styles may be set with style-src).
 /// </summary>
 public class StyleSourceAttrDirectiveBuilder : CspDirectiveBuilder
 {
