54185de6053f7406888c25a4c8f1542f4f1fa724
RaceProUK <RaceProUK@users.noreply.github.com>
2024-03-19 20:41:10 +0000

    Added support for `Cross-Origin-Embedder-Policy: credentialless` (#153)

---
+++ src/NetEscapades.AspNetCore.SecurityHeaders/Headers/CrossOriginPolicies/EmbedderPolicy/CredentiallessDirectiveBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace NetEscapades.AspNetCore.SecurityHeaders.Headers.CrossOriginPolicies.EmbedderPolicy;
+
+/// <summary>
+/// no-cors cross-origin requests are sent without credentials.
+/// In particular, it means Cookies are omitted from the request, and ignored from the response.
+/// The responses are allowed without an explicit permission via the Cross-Origin-Resource-Policy header.
+/// Navigate responses behave similarly as the require-corp mode: They require Cross-Origin-Resource-Policy response header.
+/// From: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Embedder-Policy#directives
+/// </summary>
+public class CredentiallessDirectiveBuilder : CrossOriginEmbedderPolicyDirectiveBuilderBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CredentiallessDirectiveBuilder"/> class.
+    /// </summary>
+    public CredentiallessDirectiveBuilder() : base("credentialless")
+    {
+    }
+
+    /// <inheritdoc />
+    internal override Func<HttpContext, string> CreateBuilder()
+    {
+        return ctx => Directive;
+    }
+}
