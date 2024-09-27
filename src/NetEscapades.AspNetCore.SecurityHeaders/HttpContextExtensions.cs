f91f7bc9ecda9c4caed06fa06c4b1033fd33ea59
Andrew Lock <andrewlock.net@gmail.com>
2024-09-27 21:16:00 +0100

    Make nonce generation lazy

--- src/NetEscapades.AspNetCore.SecurityHeaders/HttpContextExtensions.cs
+++ src/NetEscapades.AspNetCore.SecurityHeaders/HttpContextExtensions.cs
@@ -18,22 +18,13 @@ public static class HttpContextExtensions
     /// <returns>The nonce for the request, as a string</returns>
     public static string GetNonce(this HttpContext context)
     {
-        return context.Items[Constants.DefaultNonceKey] as string ?? string.Empty;
-    }
-
-    /// <summary>
-    /// Fetch the nonce (number used once) for the request
-    /// </summary>
-    /// <param name="context">The <see cref="HttpContext"/> for the request</param>
-    /// <param name="nonce">The nonce to set for the request</param>
-    internal static void SetNonce(this HttpContext context, string nonce)
-    {
-        if (string.IsNullOrEmpty(nonce))
+        if (context.Items[Constants.DefaultNonceKey] is not string nonce)
         {
-            throw new ArgumentException("Value cannot be null or empty.", nameof(nonce));
+            nonce = NonceGenerator.CreateNonce();
+            context.Items[Constants.DefaultNonceKey] = nonce;
         }
 
-        context.Items[Constants.DefaultNonceKey] = nonce;
+        return nonce;
     }
 
     /// <summary>
