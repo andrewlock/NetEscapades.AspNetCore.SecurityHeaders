5f7b6264fee71db20fbff3af19992f52236eef95
Andrew Lock <andrewlock.net@gmail.com>
2025-03-11 20:37:59 +0000

    Bump version (#230)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,16 @@
 # Changelog
 
+## [v1.0.0-preview.4]
+
+Build updates:
+
+* Fix recording test results #221
+* Define version in the build project instead #223
+* Generate SBOM #222
+* Generate SBOM attestation #224
+* Generate artifact provenance attestation #225
+* Automatically create releases #229
+
 ## [v1.0.0-preview.3]
 
 Features:
--- build/Build.cs
+++ build/Build.cs
@@ -22,7 +22,7 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    readonly string Version = "1.0.0-preview.3"; 
+    readonly string Version = "1.0.0-preview.4"; 
     
     [Solution(GenerateProjects = true)] readonly Solution Solution;
 
